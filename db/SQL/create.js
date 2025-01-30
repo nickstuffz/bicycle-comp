320776367f25e6331fc921a4c67194f26bb8548e
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2025-01-29 16:44:39 -0800

    change table data structure

--- db/SQL/create.js
+++ db/SQL/create.js
@@ -8,17 +8,26 @@ CREATE TABLE categories (
 CREATE TABLE components (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     code VARCHAR(50) UNIQUE NOT NULL,
-    category_id INT NOT NULL REFERENCES categories (id) ON DELETE RESTRICT,
     link VARCHAR(255)
 );
 
-CREATE TABLE compatibility (
+CREATE TABLE pods (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(10) UNIQUE NOT NULL,
+    category_id INT NOT NULL REFERENCES categories (id) ON DELETE RESTRICT
+);
+
+CREATE TABLE pod_components (
+    pod_id INT NOT NULL REFERENCES pods (id) ON DELETE CASCADE,
     component_id INT NOT NULL REFERENCES components (id) ON DELETE CASCADE,
-    compatible_id INT NOT NULL REFERENCES components (id) ON DELETE CASCADE,
-    component_note VARCHAR(255),
-    compatible_note VARCHAR(255),
-    UNIQUE (component_id, compatible_id)
+    PRIMARY KEY (pod_id, component_id)
+);
+
+CREATE TABLE pod_compatibility (
+    pod1_id INT NOT NULL REFERENCES pods (id) ON DELETE CASCADE,
+    pod2_id INT NOT NULL REFERENCES pods (id) ON DELETE CASCADE,
+    PRIMARY KEY (pod1_id, pod2_id),
+    CHECK (pod1_id < pod2_id)
 );
 
 `;
