c1439ce775159b2e7ebbd807da22e257c3e14eae
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2025-02-10 02:31:48 -0800

    Super basic root route.

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ import { componentRouter } from "./routes/componentRouter.js";
 const app = express();
 
 // routes
+app.get("/", (req, res) => {
+  res.send(
+    "Hello! Welcome to the bicycle component compatibility API. This is a WIP. This app is currently based on Shimano's 2024-2025 v30 compatibilty information data. In order to use, query a URL as such: /api/component?code=ST-9000",
+  );
+});
 app.use("/api/component", componentRouter);
 
 // error handling
